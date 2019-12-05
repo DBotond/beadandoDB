0f7697dd9466330a04f84033676be32757c2540d
DBotond <dbotond89@gmail.com>
2019-12-05 18:12:45 +0100

    added exceptioncontroller to controller

---
+++ controller/src/main/java/controllers/ExceptionController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String jsanMappingException(Exception e){
+        return e.getMessage();
+    }
+}
